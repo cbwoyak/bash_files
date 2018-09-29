673ccdb42a23d0aeb187b0a93a2a835b8f5a1d93
Christopher Woyak <cbwoyak@gmail.com>
2018-09-28 22:02:18 -0400

    Check for existence of TERM_PROGRAM environment variable.

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ done
 
 # if we are on MacOS, copy bash profile, since 
 # this is read at the start of each shell
-if [ $TERM_PROGRAM = "Apple_Terminal" ]
+if [ $TERM_PROGRAM ] && [ $TERM_PROGRAM = "Apple_Terminal" ]
 then
   cp bash_profile ~/.bash_profile
 fi
