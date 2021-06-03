c7ab419a8e046acd4c7d4bb695e9937dc41f319f
Christopher Woyak <cbwoyak@gmail.com>
2021-06-02 20:18:48 -0400

    Add terminal colors for limited color terminals

--- bashrc
+++ bashrc
@@ -2,3 +2,23 @@ if [ -r ~/.bash_aliases ]
 then
   . ~/.bash_aliases
 fi
+
+if [ "$TERM" = "linux" ]; then
+  echo -en "\e]P0333333" #black {40, 00:30?} [background]
+  echo -en "\e]P1993333" #red {00:31,41}[SETUID (/usr/bin/sudo)]
+  echo -en "\e]P2339933" #green {00:32, 42} [?]
+  echo -en "\e]P3999933" #yellow {00:33} [?]
+  echo -en "\e]P433CCFF" #blue {00:34} [?]
+  echo -en "\e]P5FF99FF" #magenta {00:35} [?]A
+  echo -en "\e]P600CCCC" #cyan {00:36} [Audio]
+  echo -en "\e]P7AAAAAA" #white {00:37}[foreground]
+  echo -en "\e]P8000000" #bright black {01:30} [?]
+  echo -en "\e]P9CC3333" #bright red {01:31} [Archives]
+  echo -en "\e]PA33FF33" #bright green {01:32} [Executables]
+  echo -en "\e]PBFFFF33" #bright yellow {01:33} [Block devices]
+  echo -en "\e]PC99CCFF" #bright blue {01:34} [Directories]
+  echo -en "\e]PDFF33FF" #bright magenta {01:35} [Images, Video]
+  echo -en "\e]PE99FFFF" #bright cyan {01:36} [Softlinks]
+  echo -en "\e]PFCCCCCC" #bright white {01:37} [?]
+  clear #for background artifacting
+fi
--- install.sh
+++ install.sh
@@ -1,4 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+Help() {
+  echo "Installs the .bashrc, .bash_profile, and .bash_aliases files"
+  echo
+  echo "Syntax: install.sh [-a|p|r]"
+  echo
+  echo "Options:"
+  echo
+  echo "  -a, --aliases-only      Install only the .bash_aliases file"
+  echo "  -p, --profile-only      Install only the .bash_profile file"
+  echo "  -a, --bashrc-only       Install only the .bashrc file"
+
+}
+
+inst_rc=1
+inst_aliases=1
+inst_profile=1
 
 while [[ $# -gt 0 ]]
 do
@@ -8,7 +25,20 @@ do
       echo "Help not available"
       exit 0
       ;;
+    -a|--aliases-only)
+      inst_rc=0
+      inst_profile=0
+      ;;
+    -p|--profile-only)
+      inst_aliases=0
+      inst_bashrc=0
+      ;;
+    -r|--bashrc-only)
+      inst_aliases=0
+      inst_profile=0
+      ;;
   esac
+  shift
 done
 
 backup=~/.bash_bak
@@ -21,22 +51,31 @@ fi
 mkdir $backup
 if [[ -d $backup ]]
 then
-  if [[ -f ~/.bashrc ]]
+  if [[ (($inst_bashrc == 1)) ]]
   then
-    mv ~/.bashrc $backup
+    if [[ -f ~/.bashrc ]]
+    then
+      mv ~/.bashrc $backup
+    fi
+    cp bashrc ~/.bashrc
   fi
 
-  if [[ -f ~/.bash_aliases ]]
+  if [[ (($inst_aliases == 1)) ]]
   then
-    mv ~/.bash_aliases $backup
+    if [[ -f ~/.bash_aliases ]]
+    then
+      mv ~/.bash_aliases $backup
+    fi
+    cp bash_aliases ~/.bash_aliases
   fi
 
-  if [[ -f ~/.bash_profile ]]
+  if [[ (($inst_profile == 1)) ]]
   then
-    mv ~/.bash_profile $backup
+    if [[ -f ~/.bash_profile ]]
+    then
+      mv ~/.bash_profile $backup
+      cp bash_profile ~/.bash_profile
+    fi
   fi
 
-  cp bashrc ~/.bashrc
-  cp bash_aliases ~/.bash_aliases
-  cp bash_profile ~/.bash_profile
 fi
